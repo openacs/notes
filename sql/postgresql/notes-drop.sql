b52232816887c5a1e82f8fe3dbd60409116e3239
Vinod Kurup <vinod@kurup.org>
2001-07-09 15:37:38 +0000

    removed aD code

--- sql/postgresql/notes-drop.sql
+++ sql/postgresql/notes-drop.sql
@@ -1,3 +1,8 @@
+-- notes
+-- drop script
+-- Vinod Kurup, vkurup@massmed.org
+--
+
 --drop functions
 drop function note__new (integer,integer,varchar,varchar,varchar,timestamp,integer,varchar,integer);
 drop function note__delete (integer);
@@ -6,7 +11,21 @@ drop function note__delete (integer);
 delete from acs_permissions where object_id in (select note_id from notes);
 
 --drop objects
-delete from acs_objects where object_type='note';
+create function inline_0 ()
+returns integer as '
+declare
+	object_rec		record;
+begin
+	for object_rec in select object_id from acs_objects where object_type=''note''
+	loop
+		perform acs_object__delete( object_rec.object_id );
+	end loop;
+
+	return 0;
+end;' language 'plpgsql';
+
+select inline_0();
+drop function inline_0();
 
 --drop table
 drop table notes;
@@ -26,6 +45,6 @@ select acs_attribute__drop_attribute (
 --drop type
 select acs_object_type__drop_type(
 	   'note',
-	   'f'
+	   't'
 	);
 
