2175610211ef7747eaa33d30f50c4c0a72e3bc01
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-26 08:56:38 +0000

    Prefer '{}' to '[list]' when creating empty lists

--- www/index.tcl
+++ www/index.tcl
@@ -14,7 +14,7 @@ ad_page_contract {
 set package_id [ad_conn package_id]
 set user_id [ad_conn user_id]
 
-set context [list]
+set context {}
 set create_p [ad_permission_p $package_id create]
 
 db_multirow notes notes {} {
