27f8f178cb79649795d21fb41b77b9951a5e5aca
Hector Romojaro <hector.romojaro@gmail.com>
2018-05-09 15:33:33 +0000

    Revert massive replacement of empty list creation sentences. The use of '[list]' instead of '{}' adds semantics that could be used for performance improvements in the future, such as using a different internal representation. There is already work in this direction, avoiding the generation of the string representation during comparison of empty strings (huge thanks to Stefan Sobernig for the pointer: https://core.tcl.tk/tcl/info/44527c632ed609c2).

--- www/index.tcl
+++ www/index.tcl
@@ -14,7 +14,7 @@ ad_page_contract {
 set package_id [ad_conn package_id]
 set user_id [ad_conn user_id]
 
-set context {}
+set context [list]
 set create_p [ad_permission_p $package_id create]
 
 db_multirow notes notes {} {
